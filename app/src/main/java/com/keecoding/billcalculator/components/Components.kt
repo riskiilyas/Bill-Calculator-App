dee387288c8fd7e3d277a28a60c983074bd1fe53
riskiilyas <riskiilyas03@gmail.com>
2022-02-26 09:24:13 +0700

    Add Input Field

--- app/src/main/java/com/keecoding/billcalculator/components/Components.kt
+++ app/src/main/java/com/keecoding/billcalculator/components/Components.kt
@@ -1,2 +1,45 @@
 package com.keecoding.billcalculator.components
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AttachMoney
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    isSingleLine: Boolean,
+    keyBoardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = labelId) },
+        leadingIcon = { Icon(imageVector = Icons.Rounded.AttachMoney , contentDescription = "")},
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyBoardType,
+            imeAction = imeAction
+        ),
+        keyboardActions = onAction
+    )
+}
