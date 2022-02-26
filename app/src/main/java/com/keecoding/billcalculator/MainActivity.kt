e895bd99fb9fa398469674451d274e11d1c1df93
riskiilyas <riskiilyas03@gmail.com>
2022-02-26 10:09:18 +0700

    In Progress

--- app/src/main/java/com/keecoding/billcalculator/MainActivity.kt
+++ app/src/main/java/com/keecoding/billcalculator/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.keecoding.billcalculator
 
+import android.content.Context
+import android.icu.text.UnicodeSetIterator
 import android.os.Bundle
+import android.widget.Space
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -9,20 +13,31 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.LocalTextInputService
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextInputService
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.keecoding.billcalculator.components.InputField
 import com.keecoding.billcalculator.ui.theme.BillCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +47,7 @@ class MainActivity : ComponentActivity() {
             MyApp {
                 Column {
                     BillCard()
-                    MainContent()
+                    MainContent(this@MainActivity)
                 }
            }
         }
@@ -77,8 +92,24 @@ fun BillCard(amount: Double = 0.0) {
 }
 
 @Composable
-@Preview
-fun MainContent() {
+fun MainContent(context: Context) {
+    BillForm() { billAmt ->
+        Toast.makeText(context, "$$billAmt", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotBlank()
+    }
+    val focusManager = LocalFocusManager.current
     Surface(modifier = Modifier
         .padding(2.dp)
         .padding(top = 12.dp)
@@ -89,6 +120,27 @@ fun MainContent() {
         Column(
             modifier = Modifier.padding(8.dp)
         ) {
+            InputField(
+                modifier = Modifier.fillMaxWidth(),
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    focusManager.clearFocus()
+                })
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp),
+                horizontalArrangement = Arrangement.Start) {
+                    Text(text = "Split", modifier = Modifier.align(alignment = CenterVertically))
+                    Spacer(modifier = Modifier.width(120.dp))
+
+                }
+            } else {
+                Box(modifier = Modifier)
+            }
         }
     }
 }
